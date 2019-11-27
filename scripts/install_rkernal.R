9a2757458c9e2614423ff327c04a702a2f855ec2
happyhaoyuan <47834773+happyhaoyuan@users.noreply.github.com>
2019-11-26 22:41:43 -0800

    Update install_rkernal.R

--- scripts/install_rkernal.R
+++ scripts/install_rkernal.R
@@ -1,2 +1,16 @@
 install.packages(c('repr', 'IRdisplay', 'IRkernel'), type = 'source')
 IRkernel::installspec(user = FALSE)
+
+pkgs = '
+    devtools roxygen2
+    Rcpp
+    rmarkdown DT caTools bitops
+    RJDBC jsonlite
+    data.table
+    mailR httr urltools openssl
+    ggplot2
+    lubridate
+'
+pkgs = strsplit(pkgs, '\\s')[[1]]
+pkgs = pkgs[pkgs != '']
+install.packages(pkgs, repos = 'https://cran.rstudio.org')
