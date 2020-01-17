d4ce7c6e6a7412a83363b72be13672b3d9a48c12
haoyuan <haoyuanzou@gmail.com>
2020-01-16 21:24:25 -0800

    update

--- scripts/env/R.sh
+++ scripts/env/R.sh
@@ -2,5 +2,7 @@
 
 mkdir -p /usr/local/lib/R/etc/
 cp /settings/Rprofile.site /usr/local/lib/R/etc/
-sed -i "awww-port=7000" /etc/rstudio/rserver.conf
-sed -i "asession-timeout-minutes=0" /etc/rstudio/rsession.conf
+#sed -i "1iwww-port=7000" /etc/rstudio/rserver.conf
+echo "www-port=7000" > /etc/rstudio/rserver.conf
+#sed -i "1isession-timeout-minutes=0" /etc/rstudio/rsession.conf
+echo "session-timeout-minutes=0" > /etc/rstudio/rsession.conf
